ed1a5b34a922a7661b864b46930681f293f8541b
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-15 13:04:12 -0700

    Add static method for call xeno-canto API with a promise
    Co-authored-by: Marni Sucher <suchermarni@gmail.com>
    Co-authored-by: Tim Roth <timdroth@gmail.com>

---
+++ src/bird-sounds-service.js
@@ -0,0 +1,18 @@
+export default class BirdSoundsService {
+  static getSounds(species) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const concatenatedSpecies = species.split(" ").join("+");
+      const url = `https://www.xeno-canto.org/api/2/recordings?query=${concatenatedSpecies}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      }
+      request.open("GET", url, true);
+      request.send();    
+    });
+  }
+}
