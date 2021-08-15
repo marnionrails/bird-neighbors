0cfd5153e3575269a71ae5f78fa64daae5e7cec4
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-15 14:15:52 -0700

    Add start of fn to filter the Xeno-canto API response

--- src/xenoCantoDynamicOutput.js
+++ src/xenoCantoDynamicOutput.js
@@ -1,3 +1,10 @@
+const filterXenoCantoResponse = (response) => {
+  let bestRecordingsArray = [];
+  response.recordings.forEach((recording) => {
+    if (recording.q === "A")
+ })
+}
+
 export const dynamicResponseOutput = () => {
 
 }
