3e0d4e4fab0bf32daeaec8dc1059b9e0c2259d1e
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-15 16:03:38 -0700

    Add submit fn to call xeno-canto API & display response
    Co-authored-by: Tim Roth <timdroth@gmail.com>
    Co-authored-by: Marni Sucher <suchermarni@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import BirdSoundsService from './bird-sounds-service.js'
+import BirdSoundsService from './bird-sounds-service.js';
 
+$("#userInput").submit(function() {
+  event.preventDefault();
+  console.log("submitted");
+  let species = "Accipiter cooperii"; // mocking getting species name from eBird API response
+  let promise = BirdSoundsService.getSounds(species);
+  console.log(promise);
+  promise.then(function(response) {
+    console.log(response);
+    const body = JSON.parse(response);
+    $('#outputSounds').attr("src", body.recordings[0].url);
+  }, function(error) {
+    console.log(error);
+    $('#showErrors').text(`There was an error processing your request: ${error}`);
+  });
+});
