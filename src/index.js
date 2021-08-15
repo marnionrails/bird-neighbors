61dc90aa2be80f05fbfe4a456f1371496e8b575a
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-15 13:09:35 -0700

    Rename main.js to index.js to follow new convention
    Co-authored-by: Marni Sucher <suchermarni@gmail.com>
    Co-authored-by: Tim Roth <timdroth@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import BirdSoundsService from './bird-sounds-service.js'
+
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const DotenvWebpackPlugin = require('dotenv-webpack');
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
