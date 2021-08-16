712c4ebaf9ca4c185a21857d35bedb40423f3fc0
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-15 21:24:16 -0700

    Add CORS-Anywhere proxyServer to API call to get around CORS

--- src/bird-sounds-service.js
+++ src/bird-sounds-service.js
@@ -3,7 +3,7 @@ export default class BirdSoundsService {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
       const concatenatedSpecies = species.split(" ").join("+");
-      const url = `https://www.xeno-canto.org/api/2/recordings?query=${concatenatedSpecies}`;
+      const url = `https://babysfirstproxy.herokuapp.com/https://www.xeno-canto.org/api/2/recordings?query=${concatenatedSpecies}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
