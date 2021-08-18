51faf8c4374e9842143dfb7a2556e3a4bb0a6675
marnionrails <37088724+marnionrails@users.noreply.github.com>
2021-08-17 23:53:10 -0400

    clean up code and ensure API functionality
    Co-authored-by: Alex Bertotto <ajb5206@gmail.com>
    Co-authored-by: Smita Raj <smita.raj12@gmail.com>
    Co-authored-by: Tim Roth <timdroth@gmail.com>

--- src/ebird-service.js
+++ src/ebird-service.js
@@ -1,28 +1,8 @@
-export class EbirdService {
-  static getData(location,y,m,d) {
-  
-    return fetch(`https://api.ebird.org/v2/data/obs/${location}/historic/${y}/${m}/${d}?&maxResults=15`, {
-      headers: {
-        'x-ebirdapitoken': `${process.env.API_KEY}`,
-      },
-    })
-      .then(function(response) {
-        if (!response.ok) {
-          throw Error(response.statusText);
-        }
-        return response.json();
-      })
-      .catch(function(error) {
-        return error;
-      });
-  }
-}
-
-export class NearbyService {  
+export default class NearbyService {  
   static nearby(lat, lng, rad) {
     return fetch(`https://api.ebird.org/v2/data/obs/geo/recent?lat=${lat}&lng=${lng}&&dist=${rad}&sort=date&maxResults=10`, {
       headers: {
-        'x-ebirdapitoken': `${process.env.API_KEY}`,
+        'x-ebirdapitoken': `${process.env.API_KEY2}`,
       },
     })
       .then(function(response) {
@@ -39,27 +19,3 @@ export class NearbyService {
 
 
 
-/*      headers: {
-        
-      }
-
-    }); */
-
-/* Example POST method implementation:
-async function postData(url = '', data = {}) {
-  // Default options are marked with *
-  const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
-} */
--- src/index.js
+++ src/index.js
@@ -3,23 +3,12 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import  Geocode  from './geocode.js';
-import { EbirdService, NearbyService } from './ebird-service.js';
-
-
-function getElements(response) { 
-  if (response) {
-    for(let i=0; i<=response.length; i++){
-      $('ul.showBirds').append(`<li> The birds are called ${response[i].speciesCode} </li>`);
-    }
-  } else {
-    $('.showErrors').text(`There was an error: ${response.message}`);
-  }
-}
+import NearbyService from './ebird-service.js';
 
 function getLongLatElements(response){
   if(response){
     for(let i=0; i<=response.length; i++){
-      $("ul.showNearBirds").append(`<li> The birds nearby include a ${response[i].comName} </li>`);
+      $("ul.showNearBirds").append(`<li> ${response[i].comName} </li>`);
     }
   } else {
     $('.showErrors').text(`There was an error: ${response.message}`);
@@ -27,34 +16,15 @@ function getLongLatElements(response){
 }
 
 $(document).ready(function() {
-  $('#birdLocation').click(function() {
-    let location = $('#location').val();
-    let y = $("#year").val();
-    let m = $("#month").val();
-    let d = $("#day").val();
-    EbirdService.getData(location,y,m,d)
-      .then(function(response) {
-        getElements(response);
-      });
-  });
-  $('#longlat').click(function() {
-    let lng = $('#lng').val();
-    let lat = $("#lat").val();
-    let rad = $('#rad').val();
-    
-		NearbyService.nearby(lat, lng, rad)
-      .then(function(response) {
-        getLongLatElements(response);
-      });
-  });
+  
   $('#zipcode').click(function() {
     let zipCode = $('#zipCode').val();
     let promise = Geocode.getCoordinates(zipCode);
+    $(".nearBirdHeader").text('Nearby birds include:')
+    $("ul.showNearBirds").text('');
     promise.then(function(response) {
       console.log(response);
       const body = JSON.parse(response);
-      $('#latitude').text(body.results[0].geometry.location.lat);
-      $('#longitude').text(body.results[0].geometry.location.lng);
       let lat = body.results[0].geometry.location.lat;
       let lng = body.results[0].geometry.location.lng;
       console.log(body.results[0].geometry.location.lng);
