5b591ce1d27a980c9d0f8cca4eb6e9b9a40a87c2
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-19 14:54:27 -0700

    Add passing test of DataParsing() prioritizing filteredRecordings & update syntax of previous test to match changes in code

--- tests/data-parsing.test.js
+++ tests/data-parsing.test.js
@@ -1,12 +1,16 @@
 import DataParsing from "../src/data-parsing.js";
-import { mockXCResponse1, mockXCResponse2, mockXCResponse3, mockXCResponse4, mockXCResponse5, mockXCResponse6, mockXCResponse7, mockXCResponse8, mockXCResponse9 } from "../src/mockXenoCantoResponses.js";
+import { mockXCResponse1, mockXCResponse2, mockXCResponse3, mockXCResponse4, mockXCResponse5, mockXCResponse6, mockXCResponse7, mockXCResponse8, mockXCResponse9, mockDataParsingReturn1 } from "../src/mockXenoCantoResponses.js";
 
 describe ('filterForSongs', () => {
 
   test('should correctly throw an Error when API response contains no recordings', () => {
     expect(() => {
       DataParsing.filterForSongs(mockXCResponse9);
-    }).toThrow(new Error('No relevant recordings found.'));
+    }).toThrow(new Error('No relevant recordings found at this time.'));
   });
+
+  test('should correctly prioritize filteredRecordings', () => {
+    expect(DataParsing.filterForSongs(mockXCResponse1)).toEqual(mockDataParsingReturn1);
+  })
 
 });
