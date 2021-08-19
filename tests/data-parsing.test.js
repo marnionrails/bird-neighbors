90f2c9c1837e7e1b3072a8abf755ae7f4bfc279c
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-19 16:57:48 -0700

    Add passing test for filterForSongs correctly prioritizing when filteredRecordings.length === 0 && intersection2.length === 0 &&intersection3.length >= 2

--- tests/data-parsing.test.js
+++ tests/data-parsing.test.js
@@ -1,5 +1,5 @@
 import DataParsing from "../src/data-parsing.js";
-import { mockXCResponse1, mockXCResponse2, mockXCResponse3, mockXCResponse4, mockXCResponse5, mockXCResponse6, mockXCResponse7, mockXCResponse8, mockXCResponse9, mockDataParsingReturn1, mockDataParsingReturn2, mockDataParsingReturn3, mockDataParsingReturn4 } from "../src/mockXenoCantoResponses.js";
+import { mockXCResponse1, mockXCResponse2, mockXCResponse3, mockXCResponse4, mockXCResponse5, mockXCResponse6, mockXCResponse7, mockXCResponse8, mockXCResponse9, mockDataParsingReturn1, mockDataParsingReturn2, mockDataParsingReturn3, mockDataParsingReturn4, mockDataParsingReturn5 } from "../src/mockXenoCantoResponses.js";
 
 describe ('filterForSongs', () => {
 
@@ -23,6 +23,10 @@ describe ('filterForSongs', () => {
 
   test ('should correctly prioritize when filteredRecordings.length === 0 && intersection2.length === 1 && intersection3.length >= 2', () => {
     expect(DataParsing.filterForSongs(mockXCResponse4)).toEqual(mockDataParsingReturn4);
+  });
+
+  test ('should correctly prioritize when filteredRecordings.length === 0 && intersection2.length === 0 && intersection3.length >= 2', () => {
+    expect(DataParsing.filterForSongs(mockXCResponse5)).toEqual(mockDataParsingReturn5);
   });
 
 });
