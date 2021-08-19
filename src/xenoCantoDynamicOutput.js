493e0734e5de37eddc6bedf2d6096e736362b2c1
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-18 17:58:44 -0700

    Comment out incomplete code to allow npm to build the environment

--- src/xenoCantoDynamicOutput.js
+++ src/xenoCantoDynamicOutput.js
@@ -1,11 +1,4 @@
-const filterXenoCantoResponse = (response) => {
-  let bestRecordingsArray = [];
-  response.recordings.forEach((recording) => {
-    if (recording.q === "A")
- })
-}
-
-export const dynamicResponseOutput = () => {
+// export const dynamicResponseOutput = () => {
 
 // }
 
