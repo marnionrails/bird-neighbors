644aa624c3f64a65d6a4511eb1e6853cda6283a0
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-18 20:39:53 -0700

    Add passing test of filterForSongs when API response has no recordings i.e. filterForSongs(mockXCResponse9)

--- tests/data-parsing.test.js
+++ tests/data-parsing.test.js
@@ -1,9 +1,12 @@
 import DataParsing from "../src/data-parsing.js";
-import { mockXCResponse1, mockXCResponse2, mockXCResponse3, mockXCResponse4, mockXCResponse5, mockXCResponse6, mockXCResponse7, mockXCResponse8, mockXCResponse9 } from "../src/mockXenoCantoResponses.js"
+import { mockXCResponse1, mockXCResponse2, mockXCResponse3, mockXCResponse4, mockXCResponse5, mockXCResponse6, mockXCResponse7, mockXCResponse8, mockXCResponse9 } from "../src/mockXenoCantoResponses.js";
 
 describe ('filterForSongs', () => {
 
-  test('should determine when API response contains no recordings', () => {
-    expect(DataParsing.filterForSongs(mockXCResponse9)).toEqual(Error);
+  test('should correctly throw an Error when API response contains no recordings', () => {
+    expect(() => {
+      DataParsing.filterForSongs(mockXCResponse9);
+    }).toThrow(new Error('No relevant recordings found.'));
   });
+
 });
