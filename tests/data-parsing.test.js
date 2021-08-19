9aebdbf76f66470d9229c588e9a9cb7d2e07babb
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-18 20:09:48 -0700

    Add good fail for test of filterForSongs(mockXCResponse9)

--- tests/data-parsing.test.js
+++ tests/data-parsing.test.js
@@ -1,2 +1,9 @@
 import DataParsing from "../src/data-parsing.js";
-import { mockXCResponse1, mockXCResponse2, mockXCResponse3, mockXCResponse4, mockXCResponse5, mockXCResponse6, mockXCResponse7, mockXCResponse8} from "../src/mockXenoCantoResponses.js"
+import { mockXCResponse1, mockXCResponse2, mockXCResponse3, mockXCResponse4, mockXCResponse5, mockXCResponse6, mockXCResponse7, mockXCResponse8, mockXCResponse9 } from "../src/mockXenoCantoResponses.js"
+
+describe ('filterForSongs', () => {
+
+  test('should determine when API response contains no recordings', () => {
+    expect(DataParsing.filterForSongs(mockXCResponse9)).toEqual(Error);
+  });
+});
