ad48f3925b2a07ee93fe307a457cb499f01b8625
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-18 17:56:28 -0700

    Import and call fn to filter xeno-canto API response

--- src/index.js
+++ src/index.js
@@ -3,19 +3,18 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import BirdSoundsService from './bird-sounds-service.js';
+import DataParsing from './data-parsing.js';
 
 $("#userInput").submit(function() {
   event.preventDefault();
-  console.log("submitted");
   let species = "Accipiter cooperii"; // mocking getting species name from eBird API response
   let promise = BirdSoundsService.getSounds(species);
-  console.log(promise);
   promise.then(function(response) {
-    console.log(response);
     const body = JSON.parse(response);
     $('#outputSounds').attr("src", body.recordings[0].url);
+    const recordingsToOutput = DataParsing.filterXenoCantoResponse(body);
+    console.log(recordingsToOutput);
   }, function(error) {
-    console.log(error);
     $('#showErrors').text(`There was an error processing your request: ${error}`);
   });
 });
