3343532d0d3d4a152f4b81aea71b5f29057dd5a0
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-21 10:35:05 -0700

    Add passing test of filterForSongs correctly prioritizing filteredRecordings.length === 0 && intersection2.length === 0 && intersection3.length === 0 && songRecordingsArray.length >= 2

--- tests/data-parsing.test.js
+++ tests/data-parsing.test.js
@@ -1,5 +1,5 @@
 import DataParsing from "../src/data-parsing.js";
-import { mockXCResponse1, mockXCResponse2, mockXCResponse3, mockXCResponse4, mockXCResponse5, mockXCResponse6, mockXCResponse7, mockXCResponse8, mockXCResponse9, mockDataParsingReturn1, mockDataParsingReturn2, mockDataParsingReturn3, mockDataParsingReturn4, mockDataParsingReturn5, mockDataParsingReturn6 } from "../src/mockXenoCantoResponses.js";
+import { mockXCResponse1, mockXCResponse2, mockXCResponse3, mockXCResponse4, mockXCResponse5, mockXCResponse6, mockXCResponse7, mockXCResponse8, mockXCResponse9, mockDataParsingReturn1, mockDataParsingReturn2, mockDataParsingReturn3, mockDataParsingReturn4, mockDataParsingReturn5, mockDataParsingReturn6, mockDataParsingReturn7 } from "../src/mockXenoCantoResponses.js";
 
 describe ('filterForSongs', () => {
 
@@ -29,8 +29,12 @@ describe ('filterForSongs', () => {
     expect(DataParsing.filterForSongs(mockXCResponse5)).toEqual(mockDataParsingReturn5);
   });
 
-  test ('hould correctly prioritize when filteredRecordings.length === 0 && intersection2.length === 0 && intersection3.length === 1 && songRecordingsArray.length >= 2', () => {
+  test ('should correctly prioritize when filteredRecordings.length === 0 && intersection2.length === 0 && intersection3.length === 1 && songRecordingsArray.length >= 2', () => {
     expect(DataParsing.filterForSongs(mockXCResponse6)).toEqual(mockDataParsingReturn6);
+  });
+
+  test ('should correctly prioritize when filteredRecordings.length === 0 && intersection2.length === 0 && intersection3.length === 0 && songRecordingsArray.length >= 2', () => {
+    expect(DataParsing.filterForSongs(mockXCResponse7)).toEqual(mockDataParsingReturn7);
   });
 
 });
