6e487541e8d2efac36570629da5d5b5ecdd90a72
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-23 17:52:50 -0700

    Edit error message, fix spacing syntax, remove unneeded jQuery import

--- src/validation.js
+++ src/validation.js
@@ -1,9 +1,7 @@
-import $ from 'jquery';
-
 export default class Validation{
-  static validation(zipCode){
-    if(zipCode.length !== 5){
-     throw new Error("Invalid Zipcode. Please enter valid 5-digit zip code");
+  static validation(zipCode) {
+    if (zipCode.length !== 5) {
+      throw new Error("Invalid zip code. Please enter a valid 5-digit zip code.");
     }
   }
 }
