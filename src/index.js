c885a1475fdfe654ccafeec8885bec50830b04c8
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-23 22:54:09 -0700

    Refactor code to reflect all calls via Fetch-API & to combine error handling

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import GeocodeService  from './services/geocode-service.js';
-import GeocodeErrorHandling from './geocode-error-handling.js';
 import NearbyService from './services/ebird-service.js';
 import Validation from './validation.js';
 import BirdSoundsService from './services/bird-sounds-service.js';
@@ -19,7 +18,8 @@ function listNearbyBirds(response){
   }
 }
 
-function displayBirdSounds(response) {
+function displayBirdSounds(response, commonName) {
+  $("#common-name").text(commonName);
   const songsToOutput = DataParsing.filterForSongs(response);
   const callsToOutput = DataParsing.filterForCalls(response);
   $("#sound").attr("src", songsToOutput[0].file);
@@ -31,53 +31,49 @@ function displayErrors(error) {
   $(".showErrors").text(`${error}`);
 }
   
-  $('#zipcode').click(function() {
-    let zipCode = $('#zipCode').val();
-    let lat = "";
-    let lng = "";
-    let rad = "";
-    let sciName = "";
-    let comName = "";
-    try {
-      Validation.validation(zipCode);
-    } catch(error) {
-      $(".showErrors").text(error.message);
-      throw new Error("Invalid zip code length. Zip codes must be 5-digits");
-    }
-    GeocodeService.getCoordinates(zipCode)
-      .then(function(geocodeResponse) {
-        try {
-          GeocodeErrorHandling.assessResponseStatus(geocodeResponse);
-        } catch (error) {
-          $(".showErrors").text(error.message);
-          throw new Error("Apologies. Something went wrong behind the scenes when we tried to process your zip code.");
-        }
-        lat = geocodeResponse.results[0].geometry.location.lat;
-        lng = geocodeResponse.results[0].geometry.location.lng;
-        rad = 30;
-        return NearbyService.getNearbyBirds(lat, lng, rad);
-      })
-      .catch(function(error) {
-        $('.showErrors').text(`There was an error processing your zip code: ${error}`);
-      })
-      .then(function(nearbyServiceResponse) {
-        $(".card").show();
-        $(".showErrors").hide();
-        listNearbyBirds(nearbyServiceResponse);
-        sciName = nearbyServiceResponse[0].sciName;
-        comName = nearbyServiceResponse[0].comName;
-        $("#common-name").text(comName);
-        return BirdSoundsService.getSounds(sciName);
-      })
-      .catch(function(error) {
-        $('.showErrors').text(`There was an error with getting your local birds: ${error}`);
-      })
-      .then(function(birdSoundsResponse) {
-        const birdSoundsBody = JSON.parse(birdSoundsResponse);
-        displayBirdSounds(birdSoundsBody);
-      }, function(error) {
-        $('.showErrors').text(`There was an error with processing your bird sound request: ${error}`);
-      });
-  });
-
-
+$('#zipcode').click(function() {
+  let zipCode = $('#zipCode').val();
+  let lat = "";
+  let lng = "";
+  let rad = "";
+  let sciName = "";
+  let comName = "";
+  try {
+    $(".showErrors").hide();
+    Validation.validation(zipCode);
+  } catch(error) {
+    $(".showErrors").text(error.message);
+    throw new Error("Invalid zip code length. Zip codes must be 5-digits");
+  }
+  GeocodeService.getCoordinates(zipCode)
+    .then(function(geocodeResponse) {
+      if (geocodeResponse instanceof Error) {
+        throw Error(`Geocode API error: ${geocodeResponse.message}`);
+      }
+      if (geocodeResponse.status !== "OK") {
+        throw Error(`Geocode API error: ${geocodeResponse.status}: ${geocodeResponse.error_message}`);
+      }
+      lat = geocodeResponse.results[0].geometry.location.lat;
+      lng = geocodeResponse.results[0].geometry.location.lng;
+      rad = 30;
+      return NearbyService.getNearbyBirds(lat, lng, rad);
+    })
+    .then(function(nearbyServiceResponse) {
+      if (nearbyServiceResponse instanceof Error) {
+        throw Error(`eBird API error: ${nearbyServiceResponse.message}`);
+      }
+      listNearbyBirds(nearbyServiceResponse);
+      sciName = nearbyServiceResponse[0].sciName;
+      comName = nearbyServiceResponse[0].comName;
+      return BirdSoundsService.getSounds(sciName);
+    })
+    .then(function(birdSoundsResponse) {
+      if (birdSoundsResponse instanceof Error) {
+        throw Error(`xeno-canto API error: ${birdSoundsResponse.message}`);
+      }
+      displayBirdSounds(birdSoundsResponse, comName);
+    })
+    .catch(function(error) {
+      displayErrors(error.message);
+    });
+});
