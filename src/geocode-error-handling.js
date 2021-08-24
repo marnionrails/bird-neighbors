83eb146e74a800951e0d92ed22baf981336e5194
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-23 22:54:28 -0700

    Delete unnecessary file

--- src/geocode-error-handling.js
+++
@@ -1,7 +0,0 @@
-export default class GeocodeErrorHandling {
-  static assessResponseStatus(response) {
-    if (response.status !== "OK") {
-      throw new Error(`${response.status}: ${response.error_message}`);
-    }
-  }
-}
