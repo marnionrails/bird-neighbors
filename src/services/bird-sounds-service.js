47509d6d288633d9e38488cb0f820db5864bbc2d
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-23 22:59:33 -0700

    Fix indentation errors

--- src/services/bird-sounds-service.js
+++ src/services/bird-sounds-service.js
@@ -3,13 +3,13 @@ export default class BirdSoundsService {
     const concatenatedSpecies = species.split(" ").join("+");
     return fetch(`https://babysfirstproxy.herokuapp.com/https://www.xeno-canto.org/api/2/recordings?query=${concatenatedSpecies}`)
       .then(function(response) {
-      if (!response.ok) {
-        throw Error(response.status);
-      }
-      return response.json();
-    })
-    .catch(function(error) {
-      return Error(error);
-    })
+        if (!response.ok) {
+          throw Error(response.status);
+        }
+        return response.json();
+      })
+      .catch(function(error) {
+        return Error(error);
+      });
   }
 }
