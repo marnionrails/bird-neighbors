a8cd68ccbe9cc9a382537049f03cb5c088364023
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-24 17:52:49 -0700

    Update explanation of where the code is broken

--- src/xenoCantoDynamicOutput.js
+++ src/xenoCantoDynamicOutput.js
@@ -31,9 +31,17 @@ export default class DynamicSoundDisplay {
         id="song2${concatenatedCommonName}">
       </audio>
     </figure>`;
-      //the line below is what isn't working. The string inside the jQuery parens prints out the correct string when console.logged but it isn't being recognized as a function. I suspect its because I'm trying to pass a template literal into jQuery but the internet suggests that a template literal should be okay here so I don't actually know what's wrong.
-      $(`#song1${concatenatedCommonName}`).attr("src", songsToOutput[0].file);
-      $(`#song2${concatenatedCommonName}`).attr("src", songsToOutput[1].file);
+      //the line below is what isn't working. The string inside the jQuery parens prints out the correct string when console.logged.
+      // $(`#song1${concatenatedCommonName}`).attr("src", songsToOutput[0].file);
+      // $(`#song2${concatenatedCommonName}`).attr("src", songsToOutput[1].file);
+
+      //The next 2 lines don't work and I have no idea why
+      $("#song1Red-breasted-Nuthatch").attr("src", songsToOutput[0].file);
+      $("#song2Red-breasted-Nuthatch").attr("src", songsToOutput[1].file);
+
+      // The code below correctly console.logs the files
+      console.log(songsToOutput[0].file);
+      console.log(songsToOutput[1].file);
     });
     $('#display-sounds').show();
     
