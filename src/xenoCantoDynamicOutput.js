f153148ee7483af533cec85ff23b6a78d5633852
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-08-25 11:27:49 -0700

    Update trouble shooting notes

--- src/xenoCantoDynamicOutput.js
+++ src/xenoCantoDynamicOutput.js
@@ -16,6 +16,7 @@ export default class DynamicSoundDisplay {
     const songsToOutput = DataParsing.filterForSongs(xenoCantoResponse);
     eBirdResponse.forEach((instance) => {
       const commonName = instance.comName;
+      //need to use regex below in concatenatedCommonName to filter out any non letter characters (except hyphens) that shouldn't be in an html id
       const concatenatedCommonName = commonName.split(" ").join("-");
       htmlForSongOutput += `
       <figure>
@@ -37,8 +38,11 @@ export default class DynamicSoundDisplay {
       // $(`#song2${concatenatedCommonName}`).attr("src", songsToOutput[1].file);
 
       //The next 2 lines don't work and I have no idea why
-      $("#song1Red-breasted-Nuthatch").attr("src", songsToOutput[0].file);
-      $("#song2Red-breasted-Nuthatch").attr("src", songsToOutput[1].file);
+      //they don't work even when the bird name doesn't include characters such as '
+      //I update the bird name to match whatever eBird is currently returning -- to give context this is matching the code in lines 26 & 33
+      //This is all part of trouble shooting why lines 37 and 38 don't work. There's no reason for them to work if the lines below don't work first.
+      $("#song1Anna's-Hummingbird").attr("src", songsToOutput[0].file);
+      $("#song2Anna's-Hummingbird").attr("src", songsToOutput[1].file);
 
       // The code below correctly console.logs the files
       console.log(songsToOutput[0].file);
